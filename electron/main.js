e514a4f61760d4e1cbe59faf46283fe2d9cd1553
Jonas Elgblad <jonas.elgblad@gmail.com>
2016-10-26 21:57:50 +0200

    change copy to ncp | install eectron-reload & nodemon & concurrently | change webpack dev-config

--- electron/main.js
+++ electron/main.js
@@ -4,13 +4,17 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+if (process.env.ENV !== 'production') {
+  require('electron-reload')(__dirname);
+}
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
